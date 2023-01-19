bef5a396a8263d29b15aada49416979af2ef58fa
HikiKL <105116028+HikiKL@users.noreply.github.com>
2023-01-19 19:50:51 -0300

    Add files via upload
    Um programa simples pra calcular o desconto em reais de algum produto.

---
+++ Desconto/Desconto/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Quanto é o valor do produto que você quer comprar?");
+
+int produto, desconto;
+
+while(!(int.TryParse(Console.ReadLine(), out produto)))
+{
+    Console.WriteLine("Digite um número válido");
+}
+
+Console.WriteLine("Quanto é o desconto?");
+
+
+while (!(int.TryParse(Console.ReadLine(), out desconto)))
+{
+    Console.WriteLine("Digite um número válido");
+}
+
+var Conta = produto * desconto / 100;
+var Resultado = produto - Conta; 
+
+Console.WriteLine($"O valor do produto de {produto} reais com {desconto}% de desconto é {Resultado} reais");
+
+Console.ReadKey();
+
+
